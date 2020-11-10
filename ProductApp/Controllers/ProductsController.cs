ecbdc0ac68e3cffec72e3029af2ef42aff430c1a
johnmurray4 <54542562+johnmurray4@users.noreply.github.com>
2020-11-10 12:37:04 -0500

    Update ProductsController.cs

--- ProductApp/Controllers/ProductsController.cs
+++ ProductApp/Controllers/ProductsController.cs
@@ -1,4 +1,4 @@
-﻿using ProductApp.Models;
+using ProductApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +11,7 @@ namespace ProductApp.Controllers
     {
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 1, Name = "Potato Soup", Category = "Shmoceries", Price = 22 },
             new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
             new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
         };
